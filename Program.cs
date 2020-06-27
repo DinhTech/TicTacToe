8cd07493ede9938d690febfa7007f4e02b345a7d
DinhTech <dinh_k_thai@hotmail.com>
2020-06-27 13:54:49 -0600

    Add Users guide board position.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Users guide board position.
+            Console.WriteLine("Game Board Positions:");
+            Console.WriteLine("-------------");
+            Console.WriteLine("| 1 | 2 | 3 |");
+            Console.WriteLine("-------------");
+            Console.WriteLine("| 4 | 5 | 6 |");
+            Console.WriteLine("-------------");
+            Console.WriteLine("| 7 | 8 | 9 |");
+            Console.WriteLine("-------------");
+            Console.WriteLine("Game Board Status:");
+             Console.WriteLine("-------------");
+            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("-------------");
+            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("-------------");
+            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("-------------");
+            Console.WriteLine("Player X, please enter a square number to place your token in:");
         }
     }
 }
